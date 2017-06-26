c5a3fd30fb1f4084622117fe1f2556e58b39f9dc
Jade <jade@bibaoke.com>
2017-06-26 18:41:21 +0800

    发布到 nuget

--- Less.Common/Properties/AssemblyInfo.cs
+++ Less.Common/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("Less.Common")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Less 系列基础类库")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Less.Common")]
