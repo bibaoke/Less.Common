3b776f4d7c7491c9b05373287ddace8facb6baff
bibaoke <john@bibaoke.com>
2017-10-27 09:05:22 +0800

    增加一个 IsDefault 方法的重载

--- Less.Common/Extensions/ObjectExtensions.cs
+++ Less.Common/Extensions/ObjectExtensions.cs
@@ -25,6 +25,18 @@ namespace Less
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="t"></param>
+        /// <param name="instead">替代值</param>
+        /// <returns></returns>
+        public static T IsDefault<T>(this T t, T instead) where T : struct
+        {
+            return t.IsDefault() ? instead : t;
+        }
+
+        /// <summary>
+        /// 如果是默认值
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="t"></param>
         /// <returns></returns>
         public static bool IsDefault<T>(this T t) where T : struct
         {
@@ -44,9 +56,10 @@ namespace Less
         /// <summary>
         /// 如果是 null
         /// </summary>
-        /// <typeparam name="T">类型</typeparam>
-        /// <param name="t">实例</param>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="t"></param>
         /// <param name="instead">替代实例</param>
+        /// <returns></returns>
         public static T IsNull<T>(this T t, T instead) where T : class
         {
             return t.IsNull() ? instead : t;
