4779e5b9e680b7b81d647c85952ea81d23ef5d2b
bibaoke <john@bibaoke.com>
2018-10-24 11:26:22 +0800

    添加 HtmlDocument 扩展方法

---
+++ Less.Common/Less.Windows/GUI/HtmlDocumentExtensions.cs
@@ -0,0 +1,24 @@
+﻿//bibaoke.com
+
+using System.Linq;
+using System.Windows.Forms;
+using Less.Text;
+
+namespace Less.Windows
+{
+    /// <summary>
+    /// HtmlDocument 扩展方法
+    /// </summary>
+    public static class HtmlDocumentExtensions
+    {
+        /// <summary>
+        /// 获取整个 html 文档
+        /// </summary>
+        /// <param name="document"></param>
+        /// <returns></returns>
+        public static string GetHtml(this HtmlDocument document)
+        {
+            return document.All.Cast<HtmlElement>().Where(i => i.Parent.IsNull()).Select(i => i.OuterHtml).Join();
+        }
+    }
+}
